fe957181dd58f9fe8b18ca105a74f1f75c4c6ced
Rem <son.phan@enotion.io>
2025-02-06 17:31:26 +0700

    init

---
+++ test.js
@@ -0,0 +1,29 @@
+var a = 10;
+
+if (a) {
+console.log("abc");
+}
+
+if (a) {
+console.log('bcd');
+} else {
+console.log('bcd');
+}
+
+if (a) {
+console.log("abc");
+} else {
+console.log("abc");
+}
+
+if (a) {
+console.log("abc");
+} else {
+console.log("abc");
+}
+
+if (a) {
+console.log("abc");
+} else {
+console.log("abc");
+}
