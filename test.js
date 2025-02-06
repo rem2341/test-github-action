1cbdd7333e178d00dd73f5b17bf0a5ed852777e8
Rem <son.phan@enotion.io>
2025-02-06 17:46:50 +0700

    update test.js

--- test.js
+++ test.js
@@ -1,29 +1,27 @@
 var a = 10;
 
 if (a) {
-console.log("abc");
+    console.log("abc");
 }
 
 if (a) {
-console.log('bcd');
+    console.log('bcd');
 } else {
-console.log('bcd');
+    console.log('bcd');
+}
+
+if (a) 
+    console.log("abc");
+ else {
+    console.log("abc");
 }
 
 if (a) {
-console.log("abc");
-} else {
-console.log("abc");
-}
+    console.log("abc");
+} else
+    console.log("abc");
 
-if (a) {
-console.log("abc");
-} else {
-console.log("abc");
-}
 
-if (a) {
-console.log("abc");
-} else {
-console.log("abc");
-}
+if (a)
+    console.log("abc");
+else console.log("abc");
