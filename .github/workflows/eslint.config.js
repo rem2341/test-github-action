3ada471e8c90513806d97c97bed06a4b4423520e
Rem <son.phan@enotion.io>
2025-02-06 17:38:14 +0700

    update: config

---
+++ .github/workflows/eslint.config.js
@@ -0,0 +1,13 @@
+export default [
+    {
+        files: ["**/*.js"],
+        rules: {
+            // 'nonblock-statement-body-position': [
+            //     'error', //
+            //     'below'
+            // ],
+            "brace-style": ["warn", "1tbs"],
+            'curly': "error"
+        }
+    }
+];
